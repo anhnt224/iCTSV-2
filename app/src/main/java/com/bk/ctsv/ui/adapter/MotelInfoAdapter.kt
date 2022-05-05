78d0c2046e9df40a2ff243d664feabaad07c675f
MoonPrescene <95227833+MoonPrescene@users.noreply.github.com>
2022-05-05 11:33:43 +0700

    complete layout

---
+++ app/src/main/java/com/bk/ctsv/ui/adapter/MotelInfoAdapter.kt
@@ -0,0 +1,48 @@
+package com.bk.ctsv.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bk.ctsv.R
+import com.bk.ctsv.databinding.ListItemMotelInfoBinding
+import com.bk.ctsv.models.entity.Motel
+
+class MotelInfoAdapter(
+    private val listMotel: ArrayList<Motel>,
+    val onItemMotelInfoClickListener: OnItemMotelInfoClickListener
+    ): RecyclerView.Adapter<MotelInfoAdapter.ViewHolder>() {
+    class ViewHolder(
+            val binding: ListItemMotelInfoBinding,
+            val onItemMotelInfoClickListener: OnItemMotelInfoClickListener
+        ): RecyclerView.ViewHolder(binding.root){
+        fun bindView(motel: Motel){
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding: ListItemMotelInfoBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.list_item_motel_info,
+            parent,
+            false
+        )
+
+        return ViewHolder(binding, onItemMotelInfoClickListener)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val motel = listMotel[position]
+        return holder.bindView(motel)
+    }
+
+    override fun getItemCount(): Int {
+        return listMotel.size
+    }
+
+    interface OnItemMotelInfoClickListener{
+        fun navigateToMotelInfoDetailFragment()
+    }
+}
--- app/src/main/java/com/bk/ctsv/ui/fragments/motel/SearchMotelFragment.kt
+++ app/src/main/java/com/bk/ctsv/ui/fragments/motel/SearchMotelFragment.kt
@@ -6,29 +6,59 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bk.ctsv.R
+import com.bk.ctsv.databinding.SearchMotelFragmentBinding
 import com.bk.ctsv.di.Injectable
 import com.bk.ctsv.di.ViewModelFactory
+import com.bk.ctsv.models.entity.Motel
+import com.bk.ctsv.ui.adapter.MotelInfoAdapter
 import com.bk.ctsv.ui.viewmodels.motel.SearchMotelViewModel
 import javax.inject.Inject
 
-class SearchMotelFragment : Fragment(), Injectable {
+class SearchMotelFragment : Fragment(),
+    Injectable,
+    MotelInfoAdapter.OnItemMotelInfoClickListener{
 
     private lateinit var viewModel: SearchMotelViewModel
     @Inject
     lateinit var factory: ViewModelFactory
 
+    private lateinit var binding: SearchMotelFragmentBinding
+    private var listMotel = arrayListOf<Motel>()
+    private lateinit var motelInfoAdapter: MotelInfoAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         setupViewModel()
-        return inflater.inflate(R.layout.search_motel_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater,
+            R.layout.search_motel_fragment,
+            container,
+            false)
+
+        setUpRecyclerViewMotelInfo()
+
+        return binding.root
+    }
+
+    private fun setUpRecyclerViewMotelInfo() {
+        motelInfoAdapter = MotelInfoAdapter(listMotel , this)
+        binding.recyclerViewMotelInfo.apply {
+            adapter = motelInfoAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
     }
 
 
     private fun setupViewModel(){
         viewModel = ViewModelProvider(this, factory).get(SearchMotelViewModel::class.java)
+    }
+
+    override fun navigateToMotelInfoDetailFragment() {
+        TODO("Not yet implemented")
     }
 
 }
